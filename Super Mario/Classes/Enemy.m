efd97f66a5f7797fad6ab315d85a79c5ead0c544
chenpeng <453342404@qq.com>
2018-01-06 16:42:07 +0800

    add enemy files, start to translate

---
+++ Super Mario/Classes/Enemy.m
@@ -0,0 +1,45 @@
+//
+//  Enemy.m
+//
+//  Created by : Mr.Right
+//  Project    : Super Mario
+//  Date       : 2018/1/6
+//
+//  Copyright (c) 2018年 master.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import "Enemy.h"
+
+// -----------------------------------------------------------------
+
+@implementation Enemy
+
+// -----------------------------------------------------------------
+
++ (instancetype)node
+{
+    return [[self alloc] init];
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    NSAssert(self, @"Unable to create class %@", [self class]);
+    // class initalization goes here
+    
+    
+    
+    
+    return self;
+}
+
+// -----------------------------------------------------------------
+
+@end
+
+
+
+
+
